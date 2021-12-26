22baca4d74913063a6b4e51dbf66af775b6d718d
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2021-12-26 15:47:42 -0600

    Install unplugin-icons

--- next.config.js
+++ next.config.js
@@ -3,4 +3,15 @@ const withSvgr = require('@newhighsco/next-plugin-svgr')
 /** @type {import('next').NextConfig} */
 module.exports = withSvgr({
   reactStrictMode: true,
+  webpack(config) {
+    config.plugins.push(
+      require('unplugin-icons/webpack')({
+        compiler: 'jsx',
+        jsx: 'react',
+        extension: 'jsx',
+      }),
+    );
+
+    return config;
+  },
 })
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { ReactComponent as NextLogo } from '../public/nextjs.svg'
+import IconArrowRight from '~icons/dashicons/arrow-right.jsx'
 
 const Home: NextPage = () => {
   return (
@@ -27,12 +28,12 @@ const Home: NextPage = () => {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
+            <h2>Documentation <IconArrowRight /></h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
+            <h2>Learn <IconArrowRight /></h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
@@ -40,7 +41,7 @@ const Home: NextPage = () => {
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
+            <h2>Examples <IconArrowRight /></h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
@@ -48,7 +49,7 @@ const Home: NextPage = () => {
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Deploy <IconArrowRight /></h2>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
